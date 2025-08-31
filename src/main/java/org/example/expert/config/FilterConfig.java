2ecf99bc62724416a83d67ee000c0e016555c4ca
moominIsCute <1happymoomin1@gmail.com>
2025-08-31 18:21:05 +0900

    레벨 1-1:등록한 빈을 Spring MVC에 추가, FilterConfig에 WebMvcConfigurer인터헤이스

--- src/main/java/org/example/expert/config/FilterConfig.java
+++ src/main/java/org/example/expert/config/FilterConfig.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-public class FilterConfig {
+public class FilterConfig implements WebMvcConfigurer {
 
     private final JwtUtil jwtUtil;
     private final ObjectMapper objectMapper;
@@ -24,5 +28,9 @@ public class FilterConfig {
     @Bean
     public AuthUserArgumentResolver authUserArgumentResolver() {
         return new AuthUserArgumentResolver();
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver());
     }
 }
